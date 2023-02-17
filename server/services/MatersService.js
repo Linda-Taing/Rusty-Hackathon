8235c4bbb5116ec80372a88ad2f85a7a913ef420
rtuscany23 <122835790+rtuscany23@users.noreply.github.com>
2023-02-17 13:56:43 -0700

    get and post

--- server/services/MatersService.js
+++ server/services/MatersService.js
@@ -8,34 +8,14 @@ class MatersService {
         const maters = await dbContext.Maters.find()
         return maters
     }
+
       async creatMater(materData) {
     const newMater = await dbContext.Maters.create(materData)
+    await newMater.populate('Creator','name')
     return newMater
-  }
+    }
 
 
-//   async getBirdById(birdId) {
-//     const bird = await dbContext.Birds.findById(birdId)
-//       .populate('Creator', 'name picture')
-//       .populate('watchCount')
-//     if (!bird) {
-//       throw new BadRequest("Bird not found!")
-//     }
-//     return bird
-//   }
-
-//   async createBird(birdData) {
-//     const newBird = await dbContext.Birds.create(birdData)
-//     await newBird.populate('Creator watchCount', 'name picture')
-//     return newBird
-//   }
-
-//   async getAllBirds() {
-//     const birds = await dbContext.Birds.find()
-//       .populate('Creator', 'name picture')
-//       .populate('watchCount')
-//     return birds
-//   }
 
 
 
