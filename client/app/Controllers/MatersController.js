02a93359b7f3ee8e7ed157dbd20e3a7b105eba76
Jacob <Jacobzeme8@gmail.com>
2023-02-17 14:58:15 -0700

    maters drawing to screen

--- client/app/Controllers/MatersController.js
+++ client/app/Controllers/MatersController.js
@@ -1,14 +1,20 @@
 import { appState } from "../AppState.js";
 import { matersService } from "../Services/MatersService.js";
 import { getFormData } from "../Utils/FormHandler.js";
+import { setHTML } from "../Utils/Writer.js";
 
 
-
+function _drawMaters(){
+  let template = " "
+  appState.maters.forEach(m => template += m.ProfileTemplate)
+  setHTML('foreign-profiles', template)
+}
 
 export class MatersController{
 
   constructor(){
     this.getMaters()
+    appState.on('maters', _drawMaters)
   }
 
   async getMaters(){
