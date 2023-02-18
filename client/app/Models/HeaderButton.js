881a23a7a125132644b0c24b58cbbad5884ccc49
Jonah <jonah.osito@icloud.com>
2023-02-17 18:19:43 -0700

    i dont even remember what i did

---
+++ client/app/Models/HeaderButton.js
@@ -0,0 +1,28 @@
+import { Mater } from "./Mater"
+
+export class HeaderButton{
+
+
+    get headerButton(){
+        return /*html*/` 
+        ${Mater.}
+        `
+    }
+
+    get createProfile(){
+        return /*html*/`
+                <button type="button" class="user-profile-button-edit" data-bs-toggle="modal" data-bs-target="#createModal">
+            Create Profile
+                </button>
+        `
+    } 
+
+    get yourProfile(){
+        return /*html*/`
+        <button class="user-profile-button bg-gradient" type="button" data-bs-toggle="offcanvas" data-bs-target="#user-profile" aria-controls="offcanvasExample">
+        Your Profile
+        </button>
+        `
+    }
+    
+}
