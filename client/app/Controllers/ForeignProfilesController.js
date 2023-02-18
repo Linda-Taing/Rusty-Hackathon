a3a8883dcfd5d65a0c082163768dd566b7167b21
Jonah <jonah.osito@icloud.com>
2023-02-17 21:47:53 -0700

    commit

--- client/app/Controllers/ForeignProfilesController.js
+++ client/app/Controllers/ForeignProfilesController.js
@@ -1,12 +1,13 @@
 import { appState } from "../AppState.js";
 import { ForeignProfiles } from "../Models/ForeignProfiles.js";
+import { Mater } from "../Models/Mater.js";
 import { foreignProfilesService } from "../Services/ForeignProfilesService.js";
 import { Pop } from "../Utils/Pop.js";
 import { setHTML } from "../Utils/Writer.js";
 
 
 function _drawActiveMater(){
-    setHTML("activeMater", appState.foreignProfiles.activeMater)
+    setHTML("activeMater", appState.activeMater.activeMater)
 }
 
 export class ForeignProfilesController{
--- client/app/Models/Mater.js
+++ client/app/Models/Mater.js
@@ -44,6 +44,28 @@ export class Mater{
     `
   }
 
+  get activeMater(){
+    return`
+    <div class="d-flex align-content-center col-4 foreign-prof bg-gradient">
+    <div class="row">
+    <div class="col-12">
+        <img src="${this.img}" alt="" class="foreign-prof-img">
+    </div>
+    <div class="col-12">
+        <h3>${this.name}</h3>
+        <h3>${this.language}</h3>
+        <h3>Volts: ${this.voltage}v</h3>
+        <h3 class="bio-class">${this.description}</h3>
+    </div>
+    <div class="col-12 d-flex justify-content-between mt-5">
+        <!-- NOTE possibly have an "onhover" where the heart becomes a solid icon? (might be a jonah thing) -->
+        <h1 class="mdi mdi-heart-outline"></h1>
+    </div>
+    </div>
+</div>
+    `
+}
+
   get MaterProfile(){
     return`
     <h5>${this.name}</h5>
