12e849d04fec7ac212ad0af1c125ed4efd3d40bf
Jonah <jonah.osito@icloud.com>
2023-02-17 21:12:40 -0700

    active stuff

--- client/app/Controllers/ForeignProfilesController.js
+++ client/app/Controllers/ForeignProfilesController.js
@@ -1,11 +1,25 @@
+import { appState } from "../AppState.js";
+import { foreignProfilesService } from "../Services/ForeignProfilesService.js";
+import { Pop } from "../Utils/Pop.js";
+import { setHTML } from "../Utils/Writer.js";
 
+
+function _drawActiveMater(){
+    setHTML("activeMater", appState.activeMater.activeMater)
+}
 
 export class ForeignProfilesController{
     constructor(){
-        
+        appState.on('activeMater', _drawActiveMater)
     }
     
-    activeMater(materId){
+    async activeMater(materId){
         console.log('active mater', materId);
+        try {
+            await foreignProfilesService.activeMater(materId)
+        } catch (error) {
+            console.error(error)
+            Pop.error(error)
+        }
     }
 }
